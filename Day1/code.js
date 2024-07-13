c6988bf9b4f13001c12a13582c4bc764dde91c91
Abhishek Sharma <2k22.cscys.2211373@gmail.com>
2024-07-13 10:51:27 +0530

    Day 1 Completed

---
+++ Day1/code.js
@@ -0,0 +1,43 @@
+//  Activity : 1 -> Variable Declaration
+
+// var num = 100;
+// console.log(num);
+// let num2 = 101;
+// console.log(num2);
+
+// Activity : 2 -> Constant Declaration
+
+// const check = true;
+// console.log(check);
+
+// Activity : 3 -> Data Types
+
+// let a = 10;
+// let b = "Abhishek Sharma";
+// let c = true;
+// let d = {
+//     name : "Abhishek Sharma",
+//     challenge : "JavaScript Challenge"
+// }
+// const e = new Array(1,2,3,4,5);
+// console.log(typeof(a))
+// console.log(typeof(b))
+// console.log(typeof(c))
+// console.log(typeof(d))
+// console.log(typeof(e))
+
+
+// Activity : 4 -> Reassigning variables
+
+// let temp = 10;
+// console.log(temp);
+// temp = 12;
+// console.log(temp);
+
+// Activity : 5 -> Understanding const
+
+// const per = 20;
+// per = 21;
+// console.log(per);
+
+// error observed - Type Error -> Assignment to constant variables
