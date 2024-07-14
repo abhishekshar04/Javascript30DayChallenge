a48908da1a9913781878657a137352a2518ee338
Abhishek Sharma <2k22.cscys.2211373@gmail.com>
2024-07-14 19:52:42 +0530

    Day 2 Completed

---
+++ Day 2/code.js
@@ -0,0 +1,52 @@
+// Activity 1 -> Arithematic operations
+
+// Task 1
+// let a= 2
+// let b= 2
+// console.log(a+b)
+
+// Task 2
+// let c=2
+// let d=4
+// console.log(c-d)
+
+// Task 3
+// let e=2
+// let f=2
+// console.log(e*f)
+
+// Task 4
+// let g=2
+// let h=4
+// console.log(g/h)
+
+// Task 5
+// console.log(g%h)
+
+// Activity 2 -> assignment operators
+
+// let a=2;
+// let b=3;
+// a+=b;
+// console.log(a);
+// b-=a;
+// console.log(b);
+
+// Activity 3 -> Comparison Operators
+
+// console.log(3>2)
+// console.log(3<2)
+// console.log(5>=5)
+// console.log(6<=7)
+// console.log(5==5)
+// console.log(5===5)
+
+// Activity 4-> Logical Operators
+
+// console.log( true && false)
+// console.log( false || true)
+// console.log(!true)
+
+// Activity 5 -> Ternary Operator
+
+// console.log(5%2==0?true:false)
