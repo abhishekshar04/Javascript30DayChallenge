b3e5f1c7e9f9d247b3c3682acc21b576dc5474b0
Abhishek Sharma <2k22.cscys.2211373@gmail.com>
2024-07-15 20:50:23 +0530

    Day 3 Completed

---
+++ Day3/code.js
@@ -0,0 +1,98 @@
+// Activity -1 -> If-Else Statements
+
+// Task 1
+// let n=10;
+// if(n>0){
+//     console.log("The number is positive " + n);
+// }else if(n<0){
+//     console.log("The number is negative " + n);
+// }else{
+//     console.log("The number is zero " + n);
+// }
+
+// Task 2
+// let age = 17;
+// if(age<18){
+//     console.log("The person is not eligible to vote");
+// }else{
+//     console.log("The person is eligible to vote");
+// }
+
+// Activity 2-> Nested if-else statements
+
+// Task 3
+// let a=10
+// let b=20
+// let c=30
+// if(a>b && a>c){
+//     console.log(`The number ${a} is greatest of all three.`);
+// }else if(b>a && b>c){
+//     console.log(`The number ${b} is greatest of all three.`);
+// }else{
+//     console.log(`The number ${c} is greatest of all three.`);
+// }
+
+// Activity - 3 -> Switch case
+
+// Task 4
+// switch (new Date().getDay()) {
+//     case 0:
+//         console.log("Sunday")
+//         break;
+//     case 1:
+//         console.log("Monday")
+//         break;
+//     case 2:
+//         console.log("Tuesday")
+//         break;
+//     case 3:
+//         console.log("Wednesday")
+//         break;
+//     case 4:
+//         console.log("Thursday")
+//         break;
+//     case 5:
+//         console.log("Friday")
+//         break;
+//     case 6:
+//         console.log("Saturday")
+//         break;
+//   }
+
+// Task 5
+
+// let grade = 'B'
+// switch(grade){
+//     case 'A':
+//         console.log("The grade is A");
+//         break;
+//     case 'B':
+//         console.log("The grade is B");
+//         break;
+//     case 'C':
+//         console.log("The grade is C");
+//         break;
+//     case 'D':
+//         console.log("The grade is D");
+//         break;
+//     case 'F':
+//         console.log("You have failed");
+// }
+
+// Activity 4 - Conditional Operator ->
+
+// Task 6 
+// console.log(5%2===0?"Even":"Odd");
+
+// Activity 5 - Combining Functions ->
+
+// Task 7
+// let year= 2024
+// if(year%4==0){
+//     if(year%400==0 || year%100!=0){
+//         console.log("Leap year.");
+//     }else
+//         console.log("Not leap year.")
+// }else{
+//     console.log("Not leap year.");
+// }
