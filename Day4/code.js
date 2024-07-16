715272318e1b79325db5d938ff26d4d54429b4b7
Abhishek Sharma <2k22.cscys.2211373@gmail.com>
2024-07-16 14:33:27 +0530

    Day 4 Completed

---
+++ Day4/code.js
@@ -0,0 +1,71 @@
+// Activity 1 -> For loops
+// Task 1
+// for(let i=0;i<=10;i++){
+//     console.log(i);
+// }
+
+// Task 2
+// for(let i=1;i<=10;i++){
+//     console.log(i*5);
+// }
+
+// Activity 2 -> While Loop
+
+// Task 3
+// let sum=0
+// let i=1
+// while(i<=10){
+//     sum+=i;
+//     i++;
+// }
+// console.log(sum);
+
+// Task 4
+// let i=10;
+// while(i>0){
+//     console.log(i);
+//     i--;
+// }
+
+// Activity 3 -> Do-while Loop
+// Task 5
+// let i=1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=5);
+
+// Task 6
+// let number = 5;
+// let sum = 1;
+// number--;
+// do{
+//     sum+=(sum*number);
+//     number--;
+// }while(number>=1);
+// console.log(sum);
+
+
+// Activity 4 -> Nested Loops
+// Task 7
+// for(let i=0;i<5;i++){
+//     let string =""
+//     for(let j=0;j<=i;j++){
+//         string +='*';
+//     }
+//     console.log(string);
+// }
+
+
+// Acivity 5 -> Loop Control Statements
+// Task 8
+// for(let i=1;i<=10;i++){
+//     if(i===5) continue;
+//     console.log(i);
+// }
+
+// Task 9
+// for(let i=1;i<=10;i++){
+//     if(i===7) break;
+//     console.log(i);
+// }
