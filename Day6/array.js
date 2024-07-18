c0f528fa0c46f02acbeb362d6acb0d8d411833cf
Abhishek Sharma <2k22.cscys.2211373@gmail.com>
2024-07-18 16:02:50 +0530

    Day 6 Completed.

---
+++ Day6/array.js
@@ -0,0 +1,88 @@
+// Activity 1-> Array Creation and Access
+// Task 1
+// let arr=[]
+// for(let i=0;i<5;i++){
+//     arr.push(i);
+// }
+// console.log(arr);
+
+// let arr= new Array();
+// for(let i=1;i<=5;i++){
+//     arr.push(i);
+// }
+// console.log(arr);
+
+// Task 2
+// console.log(arr[0])
+// console.log(arr[arr.length-1]);
+
+// Activity 2-> Array Methods(Basic)
+// Task 3
+// let arr = [1,2,3,4]
+// console.log("The array before is " + arr);
+// arr.push(5)
+// console.log("The array after is " + arr);
+
+// Task 4
+// arr.pop()
+// console.log("The array after is " + arr);
+
+// Task 5
+// arr.shift()
+// console.log("The array after is " + arr);
+
+// Task 6
+// arr.unshift(0);
+// console.log("The array after is " + arr);
+
+// Activity 3 -> Array Methods (Intermediate)
+// let arr = [1,2,3,4]
+// Task 7
+// let arr2=arr.map((n)=>{
+//     return n*2;
+// })
+// console.log(arr2);
+
+// Task 8
+// let arr3=arr.filter((n)=>{
+//     if(n%2==0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// });
+// console.log(arr3);
+
+// Task 9
+// let sum1 = arr.reduce((total,value)=>{
+//     return total +value;
+// })
+// console.log(sum1);
+
+// Activity 4 -> Array Iteration
+// let arr=[1,2,3,4,5,6,7]
+// Task 10
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+// Task 11
+// arr.forEach((value)=>{
+//     console.log(value);
+// })
+
+// Activity 5-> Multi Dimensional Arrays
+// Task 12
+// let matrix=[];
+// for(let i=0;i<5;i++){
+//     let arr=[];
+//     for(let j=0;j<5;j++){
+//         arr.push(j);
+//     }
+//     matrix.push(arr);
+// }
+// console.log(matrix);
+
+// Task 13
+// console.log(matrix[2][3]);
